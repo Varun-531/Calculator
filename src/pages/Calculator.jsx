1bec4f9911a0d48984c106d6276066f692300c27
Varun-531 <21pa1a0531@vishnu.edu.in>
2024-07-09 12:24:36 +0530

    Calculator code updated

--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -4,22 +4,39 @@ const Calculator = () => {
   const [input, setInput] = useState("");
   const [input2, setInput2] = useState("");
   const [result, setResult] = useState("");
+  const [operator, setOperator] = useState("");
   const handleAddition = () => {
-    setResult(parseInt(input) + parseInt(input2));
+    // setResult(parseInt(input) + parseInt(input2));
+    setOperator("+");
   };
   const handleMult = () => {
-    setResult(parseInt(input) * parseInt(input2));
+    //   setResult(parseInt(input) * parseInt(input2));
+    setOperator("+");
   };
   const handleSub = () => {
-    setResult(parseInt(input) - parseInt(input2));
+    //   setResult(parseInt(input) - parseInt(input2));
+    setOperator("-");
   };
   const handlediv = () => {
-    setResult(parseInt(input) / parseInt(input2));
+    //   setResult(parseInt(input) / parseInt(input2));
+    setOperator("/");
   };
   const handleClear = () => {
     setInput("");
     setInput2("");
     setResult("");
+    setOperator("");
+  };
+  const handleEqual = () => {
+    if (operator === "+") {
+      setResult(parseInt(input) + parseInt(input2));
+    } else if (operator === "-") {
+      setResult(parseInt(input) - parseInt(input2));
+    } else if (operator === "*") {
+      setResult(parseInt(input) * parseInt(input2));
+    } else if (operator === "/") {
+      setResult(parseInt(input) / parseInt(input2));
+    }
   };
   return (
     <div className="md:w-full flex justify-center m-10">
@@ -27,19 +44,28 @@ const Calculator = () => {
         <h1 className="text-2xl font-semibold text-center mb-4">Calculator</h1>
         <div className="flex flex-col">
           <input
-            type="text"
+            type="number"
             placeholder="0"
             value={input}
             onChange={(e) => setInput(e.target.value)}
             className="w-full p-2 my-2 border"
           />
           <input
-            type="text"
+            type="number"
             placeholder="0"
             value={input2}
             onChange={(e) => setInput2(e.target.value)}
             className="w-full p-2 my-2 border"
           />
+          <div className="flex justify-center">
+            <input
+              type="text"
+              placeholder="operator"
+              value={operator}
+              onChange={(e) => setOperator(e.target.value)}
+              className="w-1/2 p-2 my-2 border"
+            />
+          </div>
           <div className="flex flex-wrap justify-center">
             <button
               className="bg-slate-300 rounded w-[10vw] border p-2 m-2 hover:bg-slate-900 hover:text-white flex justify-center items-center"
@@ -70,6 +96,12 @@ const Calculator = () => {
               onClick={() => handleClear()}
             >
               AC
+            </button>
+            <button
+              className="bg-slate-300 rounded w-[10vw] border p-2 m-2 hover:bg-slate-900 hover:text-white flex justify-center items-center"
+              onClick={() => handleEqual()}
+            >
+              =
             </button>
           </div>
         </div>
